e9643e93896a66bf7a764f480442649d9f7b9cfa
allen <10621388@live.asia.edu.tw>
2017-12-25 22:37:53 +0800

    1225

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -4,26 +4,30 @@ import java.awt.event.ActionEvent;
 
     public class MainFrame extends JFrame{
         private Container cp;
-        private JButton jbtn1 = new JButton("猜看看");
-        private JButton jbtn2 = new JButton("RE");
-        private JButton jbtn3 = new JButton("答案");
+        private JButton jbtnr = new JButton("重新開始");
+        private JButton jbtng = new JButton("猜看看");
+        private JButton jbtna = new JButton("答案");
         private JTextArea jta = new JTextArea();
         private JPanel jpn = new JPanel();
-        private JTextField jtfkey = new JTextField("");
+        private JTextField jtf = new JTextField("");
         private int count = 0;
         public MainFrame() {
             initComp();
         }
      private void initComp(){
-         this.setBounds(100,100,500,500);
+         this.setBounds(100,100,350,250);
          this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
          cp = this.getContentPane();
-         cp.setLayout(new BorderLayout(3,3));
-         jpn.add(jbtn1);
-         jpn.add(jbtn2);
-         jpn.add(jbtn3);
-         cp.add(jpn,BorderLayout.CENTER);
-
+         cp.add(jbtnr);
+         cp.add(jbtng);
+         cp.add(jbtna);
+         cp.add(jpn);
+        jbtnr.setBounds(0,0,100,40);
+        jbtng.setBounds(120,125,80,40);
+        jbtna.setBounds(0,170,80,40);
+        jtf.setLocation(175,0);
+        jtf.setSize(100,100);
+        jpn.add(jtf);
 
 
         }
