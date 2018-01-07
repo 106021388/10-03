6366d54a829ef4a171e719fb01a949a50f0deadf
allen <10621388@live.asia.edu.tw>
2018-01-07 18:15:18 +0800

    0107

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -8,11 +8,12 @@ public class MainFrame extends JFrame{
         private JButton jbtnr = new JButton("重新猜");
         private JButton jbtng = new JButton("猜看看");
         private JButton jbtna = new JButton("答案");
-        private JTextArea jta = new JTextArea(10,20);
+        private JTextArea jta = new JTextArea(20,20);
         private JPanel jpn = new JPanel();
         private JTextField jtf = new JTextField(5);
         private final int maxInt = 99;
         private int ans = 0;
+        private int numl;
         public MainFrame() {
             initComp();
         }
@@ -24,31 +25,29 @@ public class MainFrame extends JFrame{
          cp.add(jbtng);
          cp.add(jbtna);
          cp.add(jpn);
-         jpn.add(jta);
          cp.setLayout(new GridLayout(1,4,5,5));
-        jbtnr.setBounds(0,0,100,40);
-        jbtng.setBounds(120,125,80,40);
-        jbtna.setBounds(0,170,80,40);
-        jtf.setBounds(175,0,50,50);
-        jpn.add(jtf);
-     jbtng.addActionListener(new ActionListener() {
+         jbtnr.setBounds(0,0,100,40);
+         jbtng.setBounds(120,125,80,40);
+         jbtna.setBounds(0,170,80,40);
+         jtf.setBounds(175,0,50,50);
+         jpn.add(jtf);
+         jpn.add(jta);
+         numl = (int)(Math.random()*99)+1;
+         jbtng.addActionListener(new ActionListener() {
          @Override
          public void actionPerformed(ActionEvent e) {
-             int input_ans = 0;
+             int input_ans = numl;
              try {
                  input_ans = Integer.parseInt(jtf.getText());
              } catch (Exception ex) {
-                 return;
-             }
-             if (input_ans > ans)
-             jta.append(input_ans +  "比答案小!\n");
-        else if (input_ans < ans)
-             jta.append(input_ans + "比答案大!\n");
+                 return;      }
+        if (input_ans < ans)
+             jta.append(input_ans +"比答案小!\n");
+        else if (input_ans > ans)
+             jta.append(input_ans +"比答案大!\n");
         else
             jta.append(input_ans +"你答對了!\n");
-            jtf.setText("");
-            jtf.requestFocus();
-         }
+                    }
      });
   jbtnr.addActionListener(new ActionListener() {
       @Override
